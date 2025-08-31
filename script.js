a3b63e1e696f3541e1971e21f5e773371ca843d1
RAOUFbenedine <raoufbenedine@gmail.com>
2025-08-31 17:06:16 +0100

    style done

---
+++ script.js
@@ -0,0 +1,7 @@
+//declare variable : 
+let current_num = 0 ; 
+let previous_num = 0 ; 
+let operator ; 
+//getting references : 
+//1 display : 
+let current_display = document.querySelector(".display").textContent ; 
